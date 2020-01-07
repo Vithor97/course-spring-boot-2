bdf613f98691b684082873fd8e2376edf901ac7b
Vitor Miranda <vitorsantos27@hotmail.com>
2020-01-06 22:58:37 -0300

    Exception handling - findById

--- src/main/java/com/vitor/course/services/UserService.java
+++ src/main/java/com/vitor/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.vitor.course.entities.User;
 import com.vitor.course.repositories.UserRepository;
+import com.vitor.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	//Inserir no Banco de dados
